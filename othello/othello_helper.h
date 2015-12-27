9d3154c0afb4f479833863cbd6bafc2ed14640c7
tomoex <tomoe.mailbox@gmail.com>
2015-12-27 20:52:19 +0900

    OthelloHelperを追加
    まだ作りかけ

---
+++ othello/othello_helper.h
@@ -0,0 +1,14 @@
+/*
+ * othello_helper.h
+ *
+ *  Created on: 2015/12/23
+ *      Author: tomoe
+ */
+
+#ifndef OTHELLO_HELPER_H_
+#define OTHELLO_HELPER_H_
+
+#include "othello.h"
+}
+
+#endif /* OTHELLO_HELPER_H_ */
